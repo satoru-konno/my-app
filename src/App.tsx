e1504ae7b0af910e53601667745ee5b3570b16ef
satorukonno <invisible.touch777@gmail.com>
2024-03-15 22:43:36 +0900

    文章追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
+// App.js
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  name: string;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { name: 'about Satoru Konno' };
+    // メソッドのthisをバインド
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  // handleClickメソッドを定義してください
+  handleClick(name: string){
+    this.setState({name: name});
+  }
+  
+  render() {
+    return (
+    	<div className="container">
+    	  <h3>{this.state.name}</h3>
+    	  <button onClick={() => {this.handleClick('30歳前半まで研究職をしており、主に生態系シミュレーションを中心に行ってました。その後エンジニアに転向して現在に至ります')}}>経歴</button>
+        <button onClick={() => {this.handleClick('水産環境コンサル,AIコンサル,TOEIC(905)')}}>スキル</button>
+      </div>
+    );
+  }
 }
 
 export default App;
